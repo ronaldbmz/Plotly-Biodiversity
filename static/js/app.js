71622493f591db54157b4153717f0892f969a554
ronaldbmz <tiffanyelle@Tiffanys-iMac.local>
2021-04-18 19:18:37 +0200

    build bar chart independent of dropdown filter

--- static/js/app.js
+++ static/js/app.js
@@ -14,6 +14,91 @@ d3.json("samples.json").then((importedData) => {
   console.log(html)
   d3.select(".well>select").html(html);
 
+  //*****************filter data based on selected dropdown value*****************
+  function filterData(data, inputValue) {
+		
+	  var dropdownMenu = d3.select("#selDataset");
+	  // Assign the value of the dropdown menu option to a variable
+	  var subject_id = dropdownMenu.property("value");
+	  
+	  if (subject_id === ""){subject_id = "940"}
+	
+	  return data.id === subject_id;
+	}
+    
+  samples = importedData.samples
+  console.log("samples")
+  console.log(samples)
+  var rows = samples.filter(filterData);
+  console.log(rows)
+
+  var otu_ids = rows[0].otu_ids.slice(0, 10);
+
+  console.log(otu_ids)
+  var sample_values = rows[0].sample_values.slice(0, 10);
+  console.log(sample_values)
+  var otu_labels = rows[0].otu_labels.slice(0, 10);
+  
+  var otu_ids_text = [];
+  for (var i = 0; i < otu_ids.length; i++) {
+	temp = 'OTU ' + otu_ids[i].toString()
+	otu_ids_text.push(temp)
+	}
+  console.log(otu_ids_text)
+  console.log(otu_labels)
+  
+  updatePlotlyBar(otu_ids_text.reverse(), sample_values.reverse(), otu_labels.reverse());
+
+
+    // Call getData() when a change takes place to the DOM
+    d3.selectAll("#selDataset").on("change", getData);
+    
+    // Function called by DOM changes
+    function getData() {
+        // Initialize an empty array for the country's data
+        rows = samples.filter(filterData);
+        console.log(rows)
+
+        var otu_ids = rows[0].otu_ids.slice(0, 10);
+        console.log(otu_ids)
+        var sample_values = rows[0].sample_values.slice(0, 10);
+        console.log(sample_values)
+        var otu_labels = rows[0].otu_labels.slice(0, 10);
+    
+
+    }
+
+
+// Building bar chart
+function updatePlotlyBar(x, y, hover_text) {
+		
+    var trace = {
+      x: y,
+      y: x,
+      text: hover_text,
+      type: "bar",
+      orientation: 'h'
+    };
+    
+    var Data = [trace];
+    
+    var layout = {
+          xaxis: { title: "Values"},
+          yaxis: { title: "OTU IDs"},
+          margin: {
+                l: 100,
+                r: 20,
+                t: 0,
+                b: 70
+              },
+          width: 500,
+          height: 400,
+        };
+    
+    Plotly.newPlot("bar", Data, layout);
+}
+
+
 
 })
 
